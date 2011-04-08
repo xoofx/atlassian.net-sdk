3c691acacbec6142709a11de992a645aaf59dea9
farmas <devnull@localhost>
2011-04-08 18:26:06 +1000

    Updated the StartJira.bat file (still doesn't work)

--- Atlassian.Jira.Test.Integration/StartJira.bat
+++ Atlassian.Jira.Test.Integration/StartJira.bat
@@ -1,1 +1,4 @@
-atlas-run-standalone --product jira
+atlas-run-standalone --product jira --version 4.3 --lib-plugins com.atlassian.jira.plugins:atlassian-jira-rpc-plugin:4.3 "-DdataPath=jira-plugin-test-resources-4.3-rpc-enabled.zip"
+
+
+mvn.bat com.atlassian.maven.plugins:maven-amps-plugin:3.3:run-standalone  -Dproduct=jira "-Dproduct.version=4.3" "-Dlib.plugins=com.atlassian.jira.plugins:atlassian-jira-rpc-plugin:4.3" "-DproductDataPath=C:/Atlas/dev/Atlassian.Net SDK/Atlassian.Jira.Test.Integration/jira-plugin-test-resources-4.3-rpc-enabled.zip"
