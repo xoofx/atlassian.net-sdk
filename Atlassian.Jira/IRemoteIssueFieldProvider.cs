0daee8f2837db48afab2b8d774be44ca2c1cdd3d
farmas <devnull@localhost>
2011-09-24 20:56:29 +1000

    Updated comments for accuracy.

--- Atlassian.Jira/IRemoteIssueFieldProvider.cs
+++ Atlassian.Jira/IRemoteIssueFieldProvider.cs
@@ -9,10 +9,6 @@ namespace Atlassian.Jira
     /// <summary>
     /// Represents a type that can provide RemoteFieldValues. 
     /// </summary>
-    /// <remarks>
-    /// This was created to avoid polluting the Issue type with methods used in testing. Can be removed once test assembly
-    /// has access to internals
-    /// </remarks>
     public interface IRemoteIssueFieldProvider
     {
         RemoteFieldValue[] GetRemoteFields(string fieldName);
