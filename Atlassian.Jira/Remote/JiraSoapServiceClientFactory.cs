920798625295e9fa932ad9e9648a4e928e6d5ab9
farmas <farmas@atlassian.com>
2011-12-08 15:49:58 +1100

    Initial fix to get work logging working.

--- Atlassian.Jira/Remote/JiraSoapServiceClientFactory.cs
+++ Atlassian.Jira/Remote/JiraSoapServiceClientFactory.cs
@@ -4,9 +4,83 @@ using System.Linq;
 using System.Text;
 using System.ServiceModel;
 using System.Xml;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Description;
+using System.ServiceModel.Channels;
+using System.IO;
 
 namespace Atlassian.Jira.Remote
 {
+    public class RemoteWorklogMessageInspector : IClientMessageInspector
+    {
+        public void AfterReceiveReply(ref System.ServiceModel.Channels.Message reply, object correlationState)
+        {
+            if(reply.ToString().Contains("addWorklogAndAutoAdjustRemainingEstimate"))
+            {
+                var memoryStream = new MemoryStream();
+                var writer = XmlWriter.Create(memoryStream);
+                reply.WriteMessage(writer);
+                writer.Flush();
+
+                memoryStream.Position = 0;
+                var doc = new XmlDocument();
+                doc.Load(memoryStream);
+
+                UpdateMessage(doc);
+
+                memoryStream.SetLength(0);
+                writer = XmlWriter.Create(memoryStream);
+                doc.WriteTo(writer);
+                writer.Flush();
+
+                memoryStream.Position = 0;
+                var reader = XmlReader.Create(memoryStream);
+                reply = Message.CreateMessage(reader, int.MaxValue, reply.Version);
+
+                //var newReply = Message.CreateMessage(reply.Version, null);
+                //newReply.Headers.CopyHeadersFrom(reply);
+                //newReply.Properties.CopyProperties(reply.Properties);
+
+            }   
+        }
+
+        private void UpdateMessage(XmlDocument doc)
+        {
+            var ns = new XmlNamespaceManager(doc.NameTable);
+            ns.AddNamespace("soapenv", "http://schemas.xmlsoap.org/soap/envelope/");
+            var multiRefElement = doc.SelectSingleNode("//soapenv:Body/multiRef", ns) as XmlElement;
+            multiRefElement.SetAttribute("xsi:type", "ns2:RemoteWorklog");
+            multiRefElement.SetAttribute("xmlns:ns2", "http://beans.soap.rpc.jira.atlassian.com");
+        }
+
+        public object BeforeSendRequest(ref System.ServiceModel.Channels.Message request, IClientChannel channel)
+        {
+            return null;
+        }
+    }
+
+    public class RemoteWorklogCustomBehavior : IEndpointBehavior
+    {
+        public void AddBindingParameters(ServiceEndpoint endpoint, System.ServiceModel.Channels.BindingParameterCollection bindingParameters)
+        {
+        }
+
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            clientRuntime.MessageInspectors.Add(new RemoteWorklogMessageInspector());
+        }
+
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+        }
+
+        public void Validate(ServiceEndpoint endpoint)
+        {
+        }
+    }
+
+
+
     /// <summary>
     /// Factory for JiraSoapServiceClient proxy
     /// </summary>
@@ -42,8 +116,11 @@ namespace Atlassian.Jira.Remote
             binding.Security.Message.ClientCredentialType = BasicHttpMessageCredentialType.UserName;
 
             var endpoint = new EndpointAddress(endPointUri);
+            var jiraSoapServiceClient = new JiraSoapServiceClient(binding, endpoint);
 
-            return new JiraSoapServiceClient(binding, endpoint);
+            jiraSoapServiceClient.Endpoint.Behaviors.Add(new RemoteWorklogCustomBehavior());
+
+            return jiraSoapServiceClient;
         }
     }
 }
