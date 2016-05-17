xoofx/atlassian.net-sdk

commit 28fbbdaa649788340b72951e2f058a0a00b2756e

Refactor JiraRestClient to construct the Jira object instead of using a callback to obtain it from caller.

Atlassian.Jira/Jira.cs
@@ -111,7 +111,7 @@ namespace Atlassian.Jira
             _jiraService = jiraService;
             _fileSystem = fileSystem;
             _token = accessToken;
-            _credentials = credentials;
+            _credentials = credentials ?? new JiraCredentials(null);
             _restClient = jiraService as IJiraRestClient;
             _cache = cache ?? new JiraCache();
 
@@ -120,20 +120,7 @@ namespace Atlassian.Jira
 
             if (_restClient == null && !String.IsNullOrEmpty(jiraService.Url))
             {
-                var options = new JiraRestClient.Options()
-                {
-                    Url = jiraService.Url,
-                    RestClientSettings = new JiraRestClientSettings(),
-                    GetCurrentJiraFunc = () => this
-                };
-
-                if (this._credentials != null)
-                {
-                    options.Username = _credentials.UserName;
-                    options.Password = _credentials.Password;
-                }
-
-                this._restClient = new JiraRestClient(options);
+                this._restClient = new JiraRestClient(this, jiraService.Url, _credentials);
             }
         }
 
@@ -147,20 +134,9 @@ namespace Atlassian.Jira
         /// <returns>Jira object configured to use REST API.</returns>
         public static Jira CreateRestClient(string url, string username = null, string password = null, JiraRestClientSettings settings = null)
         {
-            Jira jira = null;
-            var options = new JiraRestClient.Options()
-            {
-                Url = url,
-                Username = username,
-                Password = password,
-                RestClientSettings = settings ?? new JiraRestClientSettings(),
-                GetCurrentJiraFunc = () => jira
-            };
+            var restClient = new JiraRestClient(url, username, password, settings);
 
-            var restClient = new JiraRestClient(options);
-            jira = CreateRestClient(restClient, new JiraCredentials(username, password), options.RestClientSettings.Cache);
-
-            return jira;
+            return restClient.Jira;
         }
 
         /// <summary>

Atlassian.Jira/Remote/JiraRestClient.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Atlassian.Jira.Linq;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using RestSharp;
 using System;
@@ -16,36 +17,47 @@ namespace Atlassian.Jira.Remote
     /// </summary>
     internal class JiraRestClient : IJiraClient
     {
-        public class Options
-        {
-            public JiraRestClientSettings RestClientSettings { get; set; }
-            public string Username { get; set; }
-            public string Password { get; set; }
-            public string Url { get; set; }
-            public Func<Jira> GetCurrentJiraFunc { get; set; }
-        }
-
         private readonly RestClient _restClient;
         private readonly JiraRestClientSettings _clientSettings;
-        private readonly Func<Jira> _getCurrentJiraFunc;
+        private readonly Jira _jira;
 
         private JsonSerializerSettings _serializerSettings;
 
-        public JiraRestClient(Options options)
+        /// <summary>
+        /// Creates a new instance of JiraRestClient
+        /// </summary>
+        /// <param name="url">Url to the JIRA server.</param>
+        /// <param name="username">Username used to authenticate.</param>
+        /// <param name="password">Password used to authenticate.</param>
+        /// <param name="settings">Settings to configure the rest client.</param>
+        public JiraRestClient(string url, string username = null, string password = null, JiraRestClientSettings settings = null)
         {
-            var url = options.Url.EndsWith("/") ? options.Url : options.Url += "/";
+            url = url.EndsWith("/") ? url : url += "/";
 
-            this._clientSettings = options.RestClientSettings ?? new JiraRestClientSettings();
-            this._getCurrentJiraFunc = options.GetCurrentJiraFunc;
+            this._clientSettings = settings ?? new JiraRestClientSettings();
             this._restClient = new RestClient(url);
 
-            if (!String.IsNullOrEmpty(options.Username) && !String.IsNullOrEmpty(options.Password))
+            this._jira = new Jira(
+                new JqlExpressionVisitor(),
+                this,
+                new FileSystem(),
+                new JiraCredentials(username, password),
+                null,
+                this._clientSettings.Cache);
+
+            if (!String.IsNullOrEmpty(username) && !String.IsNullOrEmpty(password))
             {
-                this._restClient.Authenticator = new HttpBasicAuthenticator(options.Username, options.Password);
+                this._restClient.Authenticator = new HttpBasicAuthenticator(username, password);
             }
         }
 
-        public JsonSerializerSettings GetSerializerSettings()
+        internal JiraRestClient(Jira jira, string url, JiraCredentials credentials)
+            : this(url, credentials.UserName, credentials.Password, new JiraRestClientSettings())
+        {
+            this._jira = jira;
+        }
+
+        public virtual JsonSerializerSettings GetSerializerSettings()
         {
             if (this._serializerSettings == null)
             {
@@ -58,6 +70,17 @@ namespace Atlassian.Jira.Remote
             }
 
             return this._serializerSettings;
+        }
+
+        /// <summary>
+        /// Gets the Jira instance initialized with this rest client.
+        /// </summary>
+        public Jira Jira
+        {
+            get
+            {
+                return _jira;
+            }
         }
 
         #region IJiraRestClient
@@ -143,11 +166,10 @@ namespace Atlassian.Jira.Remote
 
         public Task<Issue> GetIssueAsync(string issueKey, CancellationToken token)
         {
-            var jira = this._getCurrentJiraFunc();
             var resource = String.Format("rest/api/2/issue/{0}", issueKey);
             return this.ExecuteRequestAsync<RemoteIssueWrapper>(Method.GET, resource, null, token).ContinueWith(task =>
             {
-                return new Issue(jira, task.Result.RemoteIssue);
+                return new Issue(_jira, task.Result.RemoteIssue);
             }, token, TaskContinuationOptions.None, TaskScheduler.Default);
         }
 
@@ -218,12 +240,11 @@ namespace Atlassian.Jira.Remote
 
         public Task<IPagedQueryResult<Issue>> GetIssuesFromJqlAsync(string jql, int? maxIssues, int startAt, CancellationToken token)
         {
-            var jira = this._getCurrentJiraFunc();
             var parameters = new
             {
                 jql = jql,
                 startAt = startAt,
-                maxResults = maxIssues ?? jira.MaxIssuesPerRequest,
+                maxResults = maxIssues ?? _jira.MaxIssuesPerRequest,
             };
 
             return this.ExecuteRequestAsync(Method.POST, "rest/api/2/search", parameters, token).ContinueWith<IPagedQueryResult<Issue>>(task =>
@@ -233,7 +254,7 @@ namespace Atlassian.Jira.Remote
                     .Select(issueJson =>
                     {
                         var remoteIssue = JsonConvert.DeserializeObject<RemoteIssueWrapper>(issueJson.ToString(), this.GetSerializerSettings()).RemoteIssue;
-                        return new Issue(jira, remoteIssue);
+                        return new Issue(_jira, remoteIssue);
                     });
 
                 return PagedQueryResult<Issue>.FromJson((JObject)task.Result, issues);
@@ -546,8 +567,7 @@ namespace Atlassian.Jira.Remote
             {
                 return this.ExecuteRequestAsync<RemoteProject[]>(Method.GET, "rest/api/2/project", null, token).ContinueWith(task =>
                 {
-                    var jira = this._getCurrentJiraFunc();
-                    var results = task.Result.Select(p => new Project(jira, p));
+                    var results = task.Result.Select(p => new Project(_jira, p));
                     cache.Projects.AddIfMIssing(results);
                     return results;
                 }, token, TaskContinuationOptions.None, TaskScheduler.Default);
@@ -566,11 +586,10 @@ namespace Atlassian.Jira.Remote
 
             return this.ExecuteRequestAsync(Method.GET, resource, null, token).ContinueWith(task =>
             {
-                var jira = this._getCurrentJiraFunc();
                 var attachmentsJson = task.Result["fields"]["attachment"];
                 var attachments = JsonConvert.DeserializeObject<RemoteAttachment[]>(attachmentsJson.ToString(), this.GetSerializerSettings());
 
-                return attachments.Select(remoteAttachment => new Attachment(jira, new WebClientWrapper(), remoteAttachment));
+                return attachments.Select(remoteAttachment => new Attachment(_jira, new WebClientWrapper(), remoteAttachment));
             }, token, TaskContinuationOptions.None, TaskScheduler.Default);
         }
 

commit d17529d1d153917971c8e3fd93febd81e9a85f0a

Fix regression introduced by commit 28fbbda, where Jira.isAnonymous was returning the incorrect value when no credentials were supplied.

Atlassian.Jira/Jira.cs
@@ -111,7 +111,7 @@ namespace Atlassian.Jira
             _jiraService = jiraService;
             _fileSystem = fileSystem;
             _token = accessToken;
-            _credentials = credentials ?? new JiraCredentials(null);
+            _credentials = credentials;
             _restClient = jiraService as IJiraRestClient;
             _cache = cache ?? new JiraCache();
 
@@ -120,7 +120,7 @@ namespace Atlassian.Jira
 
             if (_restClient == null && !String.IsNullOrEmpty(jiraService.Url))
             {
-                this._restClient = new JiraRestClient(this, jiraService.Url, _credentials);
+                this._restClient = new JiraRestClient(this, jiraService.Url, _credentials ?? new JiraCredentials(null));
             }
         }
 

NuGetPackage/Package.nuspec
@@ -2,7 +2,7 @@
 <package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
   <metadata>
     <id>Atlassian.SDK</id>
-    <version>6.4.0</version>
+    <version>6.4.1</version>
     <authors>Federico Silva Armas</authors>
     <owners>Federico Silva Armas</owners>
     <projectUrl>https://bitbucket.org/farmas/atlassian.net-sdk/wiki/Home</projectUrl>