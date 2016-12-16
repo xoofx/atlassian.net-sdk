e5eb46683867c8552771b9aa68b48e89ee3ebce5
Federico Silva Armas <fsilva.armas@gmail.com>
2016-12-16 03:22:06 +0000

    Refactors integration tests for Project into a single file.

--- Atlassian.Jira.Test.Integration/ProjectTest.cs
+++ Atlassian.Jira.Test.Integration/ProjectTest.cs
@@ -17,5 +17,71 @@ namespace Atlassian.Jira.Test.Integration
 
             Assert.True(issueTypes.Any());
         }
+
+        [Fact]
+        public void AddAndRemoveProjectComponent()
+        {
+            var componentName = "New Component " + _random.Next(int.MaxValue);
+            var projectInfo = new ProjectComponentCreationInfo(componentName);
+            var project = _jira.Projects.GetProjectsAsync().Result.First();
+
+            // Add a project component.
+            var component = project.AddComponentAsync(projectInfo).Result;
+            Assert.Equal(componentName, component.Name);
+
+            // Retrive project components.
+            Assert.True(project.GetComponetsAsync().Result.Any(p => p.Name == componentName));
+
+            // Delete project component
+            project.DeleteComponentAsync(component.Name).Wait();
+            Assert.False(project.GetComponetsAsync().Result.Any(p => p.Name == componentName));
+        }
+
+        [Fact]
+        public void GetProjectComponents()
+        {
+            var components = _jira.Components.GetComponentsAsync("TST").Result;
+            Assert.Equal(2, components.Count());
+        }
+
+        [Fact]
+        public void GetAndUpdateProjectVersions()
+        {
+            var startDate = new DateTime(2000, 11, 1);
+            var versions = _jira.Versions.GetVersionsAsync("TST").Result;
+            Assert.True(versions.Count() >= 3);
+
+            var version = versions.First(v => v.Name == "1.0");
+            var newDescription = "1.0 Release " + _random.Next(int.MaxValue);
+            version.Description = newDescription;
+            version.StartDate = startDate;
+            version.SaveChanges();
+
+            Assert.Equal(newDescription, version.Description);
+            version = _jira.Versions.GetVersionsAsync("TST").Result.First(v => v.Name == "1.0");
+            Assert.Equal(newDescription, version.Description);
+            Assert.Equal(version.StartDate, startDate);
+        }
+
+        [Fact]
+        public void AddAndRemoveProjectVersions()
+        {
+            var versionName = "New Version " + _random.Next(int.MaxValue);
+            var project = _jira.Projects.GetProjectsAsync().Result.First();
+            var projectInfo = new ProjectVersionCreationInfo(versionName);
+            projectInfo.StartDate = new DateTime(2000, 11, 1);
+
+            // Add a project version.
+            var version = project.AddVersionAsync(projectInfo).Result;
+            Assert.Equal(versionName, version.Name);
+            Assert.Equal(version.StartDate, projectInfo.StartDate);
+
+            // Retrive project versions.
+            Assert.True(project.GetPagedVersionsAsync().Result.Any(p => p.Name == versionName));
+
+            // Delete project version
+            project.DeleteVersionAsync(version.Name).Wait();
+            Assert.False(project.GetPagedVersionsAsync().Result.Any(p => p.Name == versionName));
+        }
     }
 }
