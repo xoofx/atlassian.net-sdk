ee77d83dd5c76c2d3d8ba958500b13b25fcaea49
Federico Silva Armas <fsilva.armas@gmail.com>
2019-09-27 12:07:19 -0700

    Adds ability to search for custom fields by issue type.
    Fixes issue #473.

--- Atlassian.Jira/IIssueFieldService.cs
+++ Atlassian.Jira/IIssueFieldService.cs
@@ -17,10 +17,18 @@ namespace Atlassian.Jira
         Task<IEnumerable<CustomField>> GetCustomFieldsAsync(CancellationToken token = default(CancellationToken));
 
         /// <summary>
+        /// Returns custom fields within JIRA given the options specified.
+        /// </summary>
+        /// <param name="options">Options to fetch custom fields.</param>
+        /// <param name="token">Cancellation token for this operation.</param>
+        Task<IEnumerable<CustomField>> GetCustomFieldsAsync(CustomFieldFetchOptions options, CancellationToken token = default);
+
+        /// <summary>
         /// Returns all custom fields within JIRA for the project specified.
         /// </summary>
         /// <param name="projectKey">The project key to retrieve all the custom fields from.</param>
         /// <param name="token">Cancellation token for this operation.</param>
+        [Obsolete("Use GetCustomFieldsAsync(options) instead.")]
         Task<IEnumerable<CustomField>> GetCustomFieldsForProjectAsync(string projectKey, CancellationToken token = default(CancellationToken));
     }
 }
