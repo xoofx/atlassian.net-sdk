57ca92cf579719994506f7a5d661c536f24e16dc
Romain Failliot <romain.failliot@ubisoft.com>
2019-10-23 11:50:05 -0400

    Small changes to improve OAuth usage
    * Fix some typos
    * Makes the token settings leaner (use get/set properties instead of
      constructors)
    * Limit to just what's necessary when generating and obtaining tokens
      with a predefined IRestClient, helps understand what's really
      necessary during that step
    * Create extension method for JiraOAuthSignatureMethod

---
+++ Atlassian.Jira/OAuth/JiraOAuthSignatureMethodExtensions.cs
@@ -0,0 +1,32 @@
+﻿using RestSharp.Authenticators.OAuth;
+
+namespace Atlassian.Jira.OAuth
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="JiraOAuthSignatureMethod"/>.
+    /// </summary>
+    public static class JiraOAuthSignatureMethodExtensions
+    {
+        /// <summary>
+        /// Converts to <see cref="OAuthSignatureMethod"/>.
+        /// </summary>
+        /// <param name="signatureMethod">The signature method.</param>
+        /// <returns>The RestSharp signature method.</returns>
+        public static OAuthSignatureMethod ToOAuthSignatureMethod(this JiraOAuthSignatureMethod signatureMethod)
+        {
+            switch (signatureMethod)
+            {
+                case JiraOAuthSignatureMethod.HmacSha1:
+                    return OAuthSignatureMethod.HmacSha1;
+                case JiraOAuthSignatureMethod.HmacSha256:
+                    return OAuthSignatureMethod.HmacSha256;
+                case JiraOAuthSignatureMethod.PlainText:
+                    return OAuthSignatureMethod.PlainText;
+                case JiraOAuthSignatureMethod.RsaSha1:
+                    return OAuthSignatureMethod.RsaSha1;
+                default:
+                    return OAuthSignatureMethod.RsaSha1;
+            }
+        }
+    }
+}
--- Atlassian.Jira/OAuth/OAuthRequestTokenSettings.cs
+++ Atlassian.Jira/OAuth/OAuthRequestTokenSettings.cs
@@ -5,32 +5,34 @@
     /// </summary>
     public class OAuthRequestTokenSettings
     {
-        public string Url;
-        public string ConsumerKey;
-        public string ConsumerSecret;
-        public string CallbackUrl;
-        public JiraOAuthSignatureMethod SignatureMethod;
-        public string RequestTokenUrl;
-        public string AuthorizeTokenUrl;
+        /// <summary>
+        /// The default relative URL to request a token.
+        /// </summary>
+        public const string DefaultRequestTokenUrl = "plugins/servlet/oauth/request-token";
+
+        /// <summary>
+        /// The default relative URL to authorize a token.
+        /// </summary>
+        public const string DefaultAuthorizeUrl = "plugins/servlet/oauth/authorize";
 
         /// <summary>
         /// Creates a Request token settings to generate a request token.
         /// </summary>
-        /// <param name="url">The url of the Jira instance to request to.</param>
+        /// <param name="url">The URL of the Jira instance to request to.</param>
         /// <param name="consumerKey">The consumer key provided by the Jira application link.</param>
         /// <param name="consumerSecret">The consumer public key in XML format.</param>
         /// <param name="callbackUrl">The callback url for the request token.</param>
         /// <param name="signatureMethod">The signature method used to sign the request.</param>
-        /// <param name="requestTokenUrl">The relative path to the url to request the token to Jira.</param>
-        /// <param name="authorizeTokenUrl">The relative path to the url to authorize the token.</param>
+        /// <param name="requestTokenUrl">The relative URL to request the token.</param>
+        /// <param name="authorizeUrl">The relative URL to authorize the token.</param>
         public OAuthRequestTokenSettings(
             string url,
             string consumerKey,
             string consumerSecret,
             string callbackUrl = null,
             JiraOAuthSignatureMethod signatureMethod = JiraOAuthSignatureMethod.RsaSha1,
-            string requestTokenUrl = "plugins/servlet/oauth/request-token",
-            string authorizeTokenUrl = "plugins/servlet/oauth/authorize")
+            string requestTokenUrl = DefaultRequestTokenUrl,
+            string authorizeUrl = DefaultAuthorizeUrl)
         {
             Url = url;
             ConsumerKey = consumerKey;
@@ -38,7 +40,42 @@
             CallbackUrl = callbackUrl;
             SignatureMethod = signatureMethod;
             RequestTokenUrl = requestTokenUrl;
-            AuthorizeTokenUrl = authorizeTokenUrl;
+            AuthorizeUrl = authorizeUrl;
         }
+
+        /// <summary>
+        /// Gets the URL of the Jira instance to request to.
+        /// </summary>
+        public string Url { get; }
+
+        /// <summary>
+        /// Gets the consumer key provided by the Jira application link.
+        /// </summary>
+        public string ConsumerKey { get; }
+
+        /// <summary>
+        /// Gets the consumer public key in XML format.
+        /// </summary>
+        public string ConsumerSecret { get; }
+
+        /// <summary>
+        /// Gets the callback URL for the request token.
+        /// </summary>
+        public string CallbackUrl { get; }
+
+        /// <summary>
+        /// Gets the signature method used to sign the request.
+        /// </summary>
+        public JiraOAuthSignatureMethod SignatureMethod { get; }
+
+        /// <summary>
+        /// Gets the relative URL to request the token.
+        /// </summary>
+        public string RequestTokenUrl { get; }
+
+        /// <summary>
+        /// Gets the relative URL to authorize the token.
+        /// </summary>
+        public string AuthorizeUrl { get; }
     }
 }
