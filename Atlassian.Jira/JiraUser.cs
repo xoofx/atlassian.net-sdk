54b8f577853651a708cc32d791ce556ba59804e5
Federico Silva Armas <fsilva.armas@gmail.com>
2020-03-08 16:17:52 -0700

    Removed Json.net annotations from JiraUser class.

--- Atlassian.Jira/JiraUser.cs
+++ Atlassian.Jira/JiraUser.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using Newtonsoft.Json;
-
-namespace Atlassian.Jira
+﻿namespace Atlassian.Jira
 {
     /// <summary>
     /// Represents a JIRA user.
@@ -11,49 +8,41 @@ namespace Atlassian.Jira
         /// <summary>
         /// The Atlassian account identifier for this user.
         /// </summary>
-        [JsonProperty("accountId")]
         public string AccountId { get; internal set; }
 
         /// <summary>
         /// The identifier for the user as defined by JIRA.
         /// </summary>
-        [JsonProperty("key")]
         public string Key { get; internal set; }
 
         /// <summary>
         /// The 'username' for the user.
         /// </summary>
-        [JsonProperty("name")]
         public string Username { get; internal set; }
 
         /// <summary>
         /// The long display name for the user.
         /// </summary>
-        [JsonProperty("displayName")]
         public string DisplayName { get; internal set; }
 
         /// <summary>
         /// The email address of the user.
         /// </summary>
-        [JsonProperty("emailAddress")]
         public string Email { get; internal set; }
 
         /// <summary>
         /// Whether the user is marked as active on the server.
         /// </summary>
-        [JsonProperty("active")]
         public bool IsActive { get; internal set; }
 
         /// <summary>
         /// The locale of the User.
         /// </summary>
-        [JsonProperty("locale")]
         public string Locale { get; internal set; }
 
         /// <summary>
         /// Url to access this resource.
         /// </summary>
-        [JsonProperty("self")]
         public string Self { get; internal set; }
 
         internal string InternalIdentifier { get; set; }
