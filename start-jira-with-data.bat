06e8c7049148414189782971dcc82425c3bace2a
Federico Silva Armas <fsilva.armas@gmail.com>
2020-12-06 10:05:09 -0800

    Updates instructions and script file to re-enable running Jira 8.5.2 by default for integration tests.

--- start-jira-with-data.bat
+++ start-jira-with-data.bat
@@ -1,7 +1,9 @@
 IF NOT "%~1"=="" (
 	SET JIRA_VERSION=%1
+) ELSE (
+	SET JIRA_VERSION=8.5.2
 )
 
 docker-compose up -d
 
-Atlassian.Jira.Test.Integration.Setup\bin\net452\JiraSetup.exe %1
+Atlassian.Jira.Test.Integration.Setup\bin\net452\JiraSetup.exe %JIRA_VERSION%
