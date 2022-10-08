6af77fe0531977c11f97231e49899e5a89cf0f8a
Romain Failliot <romain.failliot@ubisoft.com>
2022-10-08 18:54:08 +0000

    Merged in upstream-add-oauth-verifier (pull request #111)
    Add OAuth "verifier" field
    
    * Merged farmas/atlassian.net-sdk into master
    
    * Merged farmas/atlassian.net-sdk into master
    
    * Merged farmas/atlassian.net-sdk into master
    * Merged farmas/atlassian.net-sdk into master
    * Merged farmas/atlassian.net-sdk into master
    * Add OAuth verifier
    
    * Add param documentation for oAuthVerifier parameters
    
    
    Approved-by: Federico Silva Armas

---
+++ Atlassian.Jira.Test/OAuthAccessTokenTest.cs
@@ -0,0 +1,29 @@
+﻿using Atlassian.Jira.OAuth;
+using Newtonsoft.Json;
+using System;
+using Xunit;
+
+namespace Atlassian.Jira.Test
+{
+    public class OAuthAccessTokenTest
+    {
+        [Fact]
+        public void OAuthAccessToken_CanDeserialize()
+        {
+            // Arrange
+            var accessToken = new OAuthAccessToken(
+                "oauth_token",
+                "oauth_token_secret",
+                DateTimeOffset.Now);
+            var json = JsonConvert.SerializeObject(accessToken);
+
+            // Act
+            var deserializedAccessToken = JsonConvert.DeserializeObject<OAuthAccessToken>(json);
+
+            // Assert
+            Assert.Equal(accessToken.OAuthToken, deserializedAccessToken.OAuthToken);
+            Assert.Equal(accessToken.OAuthTokenSecret, deserializedAccessToken.OAuthTokenSecret);
+            Assert.Equal(accessToken.OAuthTokenExpiry, deserializedAccessToken.OAuthTokenExpiry);
+        }
+    }
+}
---
+++ Atlassian.Jira.Test/OAuthRequestTokenTest.cs
@@ -0,0 +1,30 @@
+﻿using Atlassian.Jira.OAuth;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Atlassian.Jira.Test
+{
+    public class OAuthRequestTokenTest
+    {
+        [Fact]
+        public void OAuthRequestToken_CanDeserialize()
+        {
+            // Arrange
+            var requestToken = new OAuthRequestToken(
+                "authorize_uri",
+                "oauth_token",
+                "oauth_token_secret",
+                "oauth_callback_confirmation");
+            var json = JsonConvert.SerializeObject(requestToken);
+
+            // Act
+            var deserializedRequestToken = JsonConvert.DeserializeObject<OAuthRequestToken>(json);
+
+            // Assert
+            Assert.Equal(requestToken.AuthorizeUri, deserializedRequestToken.AuthorizeUri);
+            Assert.Equal(requestToken.OAuthToken, deserializedRequestToken.OAuthToken);
+            Assert.Equal(requestToken.OAuthTokenSecret, deserializedRequestToken.OAuthTokenSecret);
+            Assert.Equal(requestToken.OAuthCallbackConfirmation, deserializedRequestToken.OAuthCallbackConfirmation);
+        }
+    }
+}
